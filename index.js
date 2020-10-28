ed568bf0b3e2a66fa6ddffa1023328fa5acd98e2
Chloe Steffens <caforonda103@yahoo.com>
2020-10-28 22:25:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ class Cat {
     this.name = name;
     this.sex = sex;
   }
-  return "${this.name} says meow!"
+  return `${this.name} says meow!`;
 }
  
 class Dog {
-  //...
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  return `${this.name} says woof!`;
 }
  
 class Bird {
-  //...
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  return `It's me! ${this.name}, the parrot! says meow!`;
 }
